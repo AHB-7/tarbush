50bb0670c0e61290d19b0c14ae5935991e322137
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-06-15 03:14:00 +0200

    google it

--- src/app/layout.js
+++ src/app/layout.js
@@ -99,10 +99,6 @@ export default function RootLayout({ children }) {
     return (
         <html lang="no">
             <Head>
-                <meta
-                    name="google-site-verification"
-                    content="AmKgMh56Lz2XNzwKxiFeEQjC_EFQTgToiCh2juC9Wxs"
-                />
                 <title>{metadata.title}</title>
                 <meta name="description" content={metadata.description} />
                 <meta name="keywords" content={metadata.keywords.join(", ")} />
