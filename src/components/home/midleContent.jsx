6ba3dde90cd479782f359a6e59fb887825d17b91
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-06-17 23:21:21 +0200

    colorfulll

--- src/components/home/midleContent.jsx
+++ src/components/home/midleContent.jsx
@@ -5,7 +5,7 @@ import { rubik } from "../../ui/font";
 
 export default function MidleBar() {
     return (
-        <div className="text-white flex flex-col items-center gap-2 backdrop-blur-md px-3 py-10 md:p-10 rounded-lg relative">
+        <div className="text-white flex flex-col items-center shadow-lg shadow-zinc-700 gap-2 backdrop-blur-sm px-3 py-10 md:p-10 rounded-lg relative">
             <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 w-full flex justify-center">
                 <motion.div
                     animate={{ rotate: [0, 10, -10, 0] }}
@@ -23,20 +23,25 @@ export default function MidleBar() {
                     />
                 </motion.div>
             </div>
-            <div className=" -z-10 absolute top-0 left-0 bg-white w-full h-full bg-opacity-20 rounded-lg"></div>
-            <h1
-                className={`${rubik.className} bg-red-400 text-nowrap font-extrabold text-4xl  md:text-6xl uppercase mt-6 md:mt-12`}
-            >
-                Velkommen til
-            </h1>
-            <h2
-                className={`${rubik.className} font-extrabold w-fit trabold text-red-400 bg-white mb-4 text-4xl md:text-6xl uppercase`}
-            >
-                Tarbush
-            </h2>
+            {/* <div className="bg-red-200 -z-10 absolute top-0 left-0 bg-brand w-full h-full bg-opacity-20 rounded-lg"></div> */}
+            <div className="bg-red-600 mb-4 rounded-lg p-4">
+                <h1
+                    className={`${rubik.className} text-nowrap font-extrabold text-4xl md:text-6xl uppercase`}
+                >
+                    Velkommen til
+                </h1>
+            </div>
+            <div className="bg-white text-red-600 mb-4 p-2 rounded-lg">
+                <h2
+                    className={`${rubik.className} font-extrabold w-fit trabold text-brand-400 text-4xl md:text-6xl uppercase`}
+                >
+                    Tarbush
+                </h2>
+            </div>
+
             <Link
                 href="/meny"
-                className=" px-6 py-2 bg-black hover:bg-white hover:text-black transition-all text-white rounded-lg shadow-inner hover:shadow-xl duration-300"
+                className="bg-gradient-to-b from-zinc-900 to-black shadow-md shadow-zinc-700 rounded-md transition-all duration-300 px-6 py-2"
             >
                 Vår Meny
             </Link>
