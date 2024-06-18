ef784de4f124efa127790cb64cdd4a4e656839ce
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-06-18 15:20:02 +0200

    nav done

--- src/components/home/sectiontwo/sectionTwo.jsx
+++ src/components/home/sectiontwo/sectionTwo.jsx
@@ -1,11 +1,11 @@
 export default function SectionTwo() {
     return (
-        <section className=" bg-red-100 w-full py-20">
-            <div className="container mx-auto">
-                <h2 className=" font-extrabold text-4xl">
+        <section className="relative w-full py-8">
+            <div className="">
+                <h2 className="font-extrabold text-4xl">
                     Gi oss en tilbakemelding og hjelp oss vokse
                 </h2>
-                <p className="text-gray-600 text-lg">
+                <p className="text-gray-600 text-lg mt-4">
                     Vi ønsker å høre din mening om oss. Gi oss en tilbakemelding
                     slik at vi kan forbedre oss.
                 </p>
