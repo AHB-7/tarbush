be2d28049685b34b0fd45b05058b6ee5eb3549e5
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-08-20 19:49:18 +0200

    studentrabatt

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -40,15 +40,34 @@ export default function Page() {
     return (
         <>
             <section className="flex items-center justify-center my-24 h-full flex-col gap-8">
-                <div className="flex items-center justify-center flex-col gap-4">
-                    <h1 className="text-4xl font-extrabold tracking-tight md:text-5xl lg:text-6xl">
-                        Bestill Nå
+                <div className="flex flex-col items-start justify-start relative -z-10 px-2">
+                    <h1 className="bg-white w-full text-center text-red-500 p-3 font-extrabold text-2xl rounded-md uppercase">
+                        Studentrabatt
                     </h1>
-                    <p className="max-w-3xl text-lg text-center font-light ">
-                        Har du spørsmål eller ønsker å bestille bord? Ta kontakt
-                        med oss i dag. Vi er her for deg!
+                    <div className="flex items-center justify-center w-full ">
+                        {" "}
+                        <Image
+                            src="/images/events/students.svg"
+                            width={500}
+                            height={400}
+                            alt="Studentrabatt for alle studentene NTNU og Trondheim tiller"
+                        ></Image>
+                    </div>
+                    <p className="py-2 text-center">
+                        Å være student er vanskelig og derfor, gleder vi oss til
+                        å gjøre det billigere for alle studentene!
                     </p>
-                    <div className="border-b pb-12 border-red-500 w-full flex items-center justify-center pt-4">
+                    <div className="flex items-center justify-center flex-col w-full h-32 relative">
+                        <p className="w-64 text-xl font-bold">
+                            20% på take away
+                        </p>
+                        <p className="w-64 text-xl font-bold">
+                            15% for å spisse inn
+                        </p>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center flex-col gap-4">
+                    <div className=" w-full flex items-center justify-center pt-4">
                         <Link
                             href="/kontakt"
                             className="px-6 relative text uppercase py-2.5 text-black bg-white shadow-md shadow-zinc-800 hover:shadow-none rounded-md transition-all duration-300  hover:text-white hover:bg-red-600"
@@ -56,54 +75,15 @@ export default function Page() {
                             Ring for å bestille
                         </Link>
                     </div>
-                </div>
-                <div className="flex flex-col items-start justify-start relative -z-10">
-                    <h1 className="bg-white text-red-500 p-3 font-extrabold text-2xl rounded-md uppercase w-fit">
-                        lunsjservering
-                    </h1>
-                    <p className="font-light text-md rounded-md text-nowrap pt-9">
-                        Servering starter mellom:{" "}
-                        <strong> 11:00 - 14-00</strong>
-                        <br />
-                        For reservasjoner: <strong>+47 929 27 015</strong>
-                        <br />
-                        Adresse: <strong>Ivar Lykkes veg 4, 7075 Tiller</strong>
-                    </p>
-                    <p className="pt-8 pe-8">
-                        Perfekt for en rask pause eller en koselig lunsj med
-                        venner og kollegaer.
-                    </p>
-                    <div className="absolute top-0 me-7 mt-7 rotate-12 right-0 md:w-20 w-10 md:h-20 h-10 bg-red-500 rounded-full flex items-center justify-center">
-                        <p className="text-white font-extrabold text-md md:text-3xl">
-                            15%
-                        </p>
+                    <div>
+                        <ul className="list-disc list-inside pb-12 px-4">
+                            <li className=" list-item">
+                                tilbudet gjelder kun for studenter med gyldig
+                                studentbevis
+                            </li>
+                            <li>tilbudet gjelder ikke alkohol</li>
+                        </ul>
                     </div>
-                    <div className="flex items-center justify-center flex-wrap w-fit gap-5 pt-8">
-                        <Image
-                            className="rounded-lg max-w-56 md:max-w-32 -rotate-90 bg-white"
-                            src="/images/mat1.png"
-                            alt="t"
-                            width="1000"
-                            height="50"
-                        />{" "}
-                        <Image
-                            className="rounded-lg max-w-32 -rotate-90 bg-white"
-                            src="/images/mat22.png"
-                            alt="t"
-                            width="1000"
-                            height="50"
-                        />{" "}
-                        <Image
-                            className="rounded-lg max-w-32 -rotate-90 bg-white"
-                            src="/images/mat3.png"
-                            alt="t"
-                            width="1000"
-                            height="50"
-                        />
-                    </div>
-                    <h2 className="bg-red-500 p-1 mt-6 font-extrabold text-normal rounded-md w-fit text-center">
-                        Vis innlegget i kassen for å få rabatten.
-                    </h2>
                 </div>
             </section>
         </>
